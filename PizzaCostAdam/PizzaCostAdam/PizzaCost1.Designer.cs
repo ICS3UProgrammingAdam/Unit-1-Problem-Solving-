12299f1d3424703774015164ab9e7a12c314121c
Adam Meskini <adam.meskini@ocsbstudent.ca>
2020-03-02 12:57:53 -0500

    added Pizza Cost

---
+++ PizzaCostAdam/PizzaCostAdam/PizzaCost1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace PizzaCostAdam
+{
+    partial class frmPizzaCost
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAsk = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblAsk
+            // 
+            this.lblAsk.AutoSize = true;
+            this.lblAsk.Location = new System.Drawing.Point(96, 69);
+            this.lblAsk.Name = "lblAsk";
+            this.lblAsk.Size = new System.Drawing.Size(0, 13);
+            this.lblAsk.TabIndex = 0;
+            // 
+            // frmPizzaCost
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAsk);
+            this.Name = "frmPizzaCost";
+            this.Text = "Pizza Cost by Adam";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAsk;
+    }
+}
+
